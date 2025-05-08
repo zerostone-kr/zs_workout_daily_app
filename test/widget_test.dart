bb6a10fcaae0d1e704d89ae27ae43a716e8b09fc
youngseock Kim <zerostone@msn.com>
2025-05-08 20:17:03 +0900

    5/8 widget_test.dart 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,12 +6,11 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:zs_workout_daily_app/main.dart';
+import 'package:zs_workout_daily_app/main.dart'; // ✅ 앱 시작 클래스 import
 
 void main() {
   testWidgets('App launches and shows calendar title', (WidgetTester tester) async {
-    await tester.pumpWidget(WorkoutDiaryApp());
+    await tester.pumpWidget(MyApp()); // ✅ 클래스명 수정
     expect(find.text('운동 일기'), findsOneWidget);
   });
 }
